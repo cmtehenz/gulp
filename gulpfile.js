b93bb7c30ec0ece8dbadfc8bd7873c3ffade167c
Gustavo Costa <cmtehenz@gmail.com>
2018-08-29 20:20:41 -0300

    gulp concat and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var pump = require('pump');
+var uglify = require('gulp-uglify');
 
 
-gulp.task('default',['sass']);
+gulp.task('default',['sass', 'compress']);
 
 gulp.task('sass', function () {
     return gulp.src('assets/src/sass/**/*.scss')
@@ -11,3 +13,12 @@ gulp.task('sass', function () {
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('assets/css'))
 });
+
+gulp.task('compress', (cb) => {
+    pump([
+        gulp.src('assets/src/js/**/*.js'),
+        concat('script.min.js'),
+        uglify(),
+        gulp.dest('assets/js')
+    ])
+})
