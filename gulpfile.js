eff0572246ab78c1f30d686df8fad3f28932c2ef
Gustavo Costa <cmtehenz@gmail.com>
2018-08-29 20:42:05 -0300

    image min

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var pump = require('pump');
 var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 
 
-gulp.task('default',['sass', 'compress']);
+gulp.task('default', ['sass', 'compress', 'image']);
 
 gulp.task('sass', function () {
     return gulp.src('assets/src/sass/**/*.scss')
         .pipe(concat('style.min.css'))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('assets/css'))
 });
 
@@ -21,4 +22,20 @@ gulp.task('compress', (cb) => {
         uglify(),
         gulp.dest('assets/js')
     ])
+})
+
+gulp.task('image', () => {
+    return gulp.src('assets/src/img/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('assets/img'))
 })
